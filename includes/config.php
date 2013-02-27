aa98ecb7a9ca930af4d3e2e5bd000a3af1958056
James Bruner <james@blueacorn.com>
2013-02-27 10:44:31 -0500

    adding config file

---
+++ includes/config.php
@@ -0,0 +1,5 @@
+<?php
+mysql_connect("localhost", "root", "robot911") or die(mysql_error());
+mysql_select_db("homepage") or die(mysql_error());
+//mysql_connect("db441883537.db.1and1.com", "dbo441883537", "g0f0rr0b0ts") or die(mysql_error());
+//mysql_select_db("db441883537") or die(mysql_error());
